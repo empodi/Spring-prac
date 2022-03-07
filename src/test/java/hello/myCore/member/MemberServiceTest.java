7b79cb6bf7a2a5dd3e314836e948920122706362
empodi <kglein9513@gmail.com>
2022-03-07 21:26:21 +0900

    MemberServiceTest

---
+++ src/test/java/hello/myCore/member/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package hello.myCore.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    // new MemberServiceImpl() !!!!
+
+    @Test
+    void join() {
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+        Assertions.assertThat(findMember.getGrade()).isEqualTo(member.getGrade());
+    }
+}
